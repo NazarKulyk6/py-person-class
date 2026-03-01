622913232f6225c7a4cad7dc577bb89640db0a85
NazarKulyk <156192686+NazarKulyk6@users.noreply.github.com>
2026-03-01 22:12:21 +0100

    Add Person class and create_person_list function
    Implemented Person class with initialization and create_person_list function to manage relationships.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    Person.people.clear()
+    result = []
+    for person_dict in people:
+        person = Person(person_dict["name"], person_dict["age"])
+        result.append(person)
+    for i, person_dict in enumerate(people):
+        if person_dict.get("wife") is not None:
+            result[i].wife = Person.people[person_dict["wife"]]
+        if person_dict.get("husband") is not None:
+            result[i].husband = Person.people[person_dict["husband"]]
+    return result
